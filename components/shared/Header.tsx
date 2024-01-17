94082ae9282bbfc99c4713447ee7431b551ee97c
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-17 11:52:47 -0500

    Editar el componente Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,11 +1,25 @@
 // rafce
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-	<div>
-	  header
-	</div>
+	<header className=' w-full border-b '>
+		<div className="wrapper flex flex-row items-center justify-between">
+			<Link href="/" className=' w-36'>
+				<Image
+					src="/assets/images/logo.svg"
+					alt='evently logo'
+					width={128}
+					height={38}	
+				/>
+			</Link>
+			<div className='flex w-32 justify-end  gap-3' >
+				Login
+			</div>
+		</div>
+	</header>
   )
 }
 
