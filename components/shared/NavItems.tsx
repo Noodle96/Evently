cbbff437f3e5260558df430f29bc840a0d2ed77b
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-19 12:26:38 -0500

    Agregar la funcionalidad de isActive un navBar y ponerlo de color Blue

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,12 +1,19 @@
 
+'use client';
 import {headerLinks} from '@/constants';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 const NavItems = () => {
+	const pathname = usePathname();
 	return (
 		<ul className="md:flex-between flex w-full md:flex-row flex-col items-start gap-5">
 			{headerLinks.map((item) => {
+				const isActive = pathname === item.route;
 				return (
-					<li>
+					<li 
+						key={item.route}
+						className={`${isActive && 'text-primary-500 '} flex-center p-medium-16 whitespace-nowrap `}
+					>
 						<Link href={item.route}>{item.label}</Link>
 					</li>
 				);
